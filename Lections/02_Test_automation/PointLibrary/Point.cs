7af217251ac29e3c08f1c4f43644c07c1dfae537
Mihail Romanov <romanov.m@skbkontur.ru>
2022-09-23 22:07:15 +0400

    Add test and samples for automation testa and unit tests

---
+++ Lections/02_Test_automation/PointLibrary/Point.cs
@@ -0,0 +1,36 @@
+﻿namespace PointLibrary
+{
+	public class Point
+	{
+		public int X
+		{
+			get;
+			private set;
+		}
+
+		public int Y
+		{
+			get;
+			private set;
+		}
+
+		public Point()
+		{
+			this.X = 0;
+			this.Y = 0;
+		}
+
+		public Point(int x, int y)
+		{
+			this.X = x;
+			this.Y = y;
+		}
+
+		public void Move(int dx, int dy)
+		{
+			this.X += dx;
+			this.Y += dy;
+		}
+	}
+
+}
---
+++ Lections/02_Test_automation/PoitLibrary.Tests/PointTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PointLibrary;
+
+namespace PoitLibrary.Tests
+{
+	[TestClass]
+	public class PointTest
+	{
+		[TestMethod]
+		public void DefaultConstructorTest()
+		{
+			Point point = new Point();
+
+			Assert.AreEqual(0, point.X);
+			Assert.AreEqual(0, point.Y);
+		}
+
+		[TestMethod]
+		public void ParametrizedConstructorTest()
+		{
+			Point point1 = new Point(0, 0);
+
+			Assert.AreEqual(0, point1.X);
+			Assert.AreEqual(0, point1.Y);
+
+			Point point2 = new Point(1, -2);
+
+			Assert.AreEqual(1, point2.X);
+			Assert.AreEqual(-2, point2.Y);
+
+		}
+
+		[TestMethod]
+		public void MoveTest()
+		{
+			Point point = new Point(1, 2);
+			point.Move(0, 0);
+
+			Assert.AreEqual(1, point.X);
+			Assert.AreEqual(2, point.Y);
+
+			point.Move(-1, -3);
+
+			Assert.AreEqual(0, point.X);
+			Assert.AreEqual(-1, point.Y);
+		}
+	}
+}
