674fc4a260c9962db4c6afd2379f521573d1b8ef
Mihail Romanov <romanov.m@skbkontur.ru>
2022-10-07 14:07:41 +0400

    Update lection

---
+++ Lections/03_Unit_tests/Library.Tests/StackTests.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Principal;
+using FluentAssertions;
+
+namespace Wintellect.PowerCollections.Tests
+{
+    [TestClass]
+    public class StackTests
+    {
+        [TestMethod]
+        public void DefaultConstructorStack()
+        {
+            Stack<int> stack = new Stack<int>(1);
+
+            Assert.AreEqual(0, stack.Count);
+            Assert.AreEqual(1, stack.Capacity);
+        }
+        [TestMethod]
+        public void CreatedStack_IsEmpty()
+        {
+            Stack<string> stack = new Stack<string>(3);
+            Assert.IsTrue(stack.IsEmpty());
+        }
+
+        [TestMethod]
+        public void AfterPushElement_Stack_IsNotEmpty()
+        {
+            Stack<string> stack = new Stack<string>(3);
+            stack.Push("hello");
+            Assert.IsFalse(stack.IsEmpty());
+        }
+
+        [TestMethod]
+        public void AfterPopElement_Stack_IsEmpty()
+        {
+            Stack<string> stack = new Stack<string>(3);
+            stack.Push("hello");
+            stack.Pop();
+            Assert.IsTrue(stack.IsEmpty());
+        }
+
+        [TestMethod]
+        public void CreatedStack_IsNotFull()
+        {
+            Stack<string> stack = new Stack<string>(3);
+            Assert.IsFalse(stack.IsFull());
+        }
+
+        [TestMethod]
+        public void AfterPushAllAvailableElements_IsFull()
+        {
+            Stack<string> stack = new Stack<string>(3);
+            stack.Push("hello");
+            stack.Push("my");
+            stack.Push("friend");
+            Assert.IsTrue(stack.IsFull());
+        }
+
+        [TestMethod]
+        public void AfterPushNotAllAvailableElements_IsNotFull()
+        {
+            Stack<string> stack = new Stack<string>(3);
+            stack.Push("hello");
+            stack.Push("my");
+            Assert.IsFalse(stack.IsFull());
+        }
+
+        [TestMethod]
+        public void AfterPopElementFromFilledStack_IsNotFull()
+        {
+            Stack<string> stack = new Stack<string>(2);
+            stack.Push("hello");
+            stack.Push("my");
+            stack.Pop();
+            Assert.IsFalse(stack.IsFull());
+        }
+
+        [TestMethod]
+        public void Push()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            stack.Push(7);
+            Assert.AreEqual(7, stack.Top());
+            Assert.AreEqual(1, stack.Count);
+        }
+
+        [TestMethod]
+        public void Pop()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            stack.Push(7);
+            Assert.AreEqual(7, stack.Pop());
+            Assert.AreEqual(0, stack.Count);
+        }
+
+        [TestMethod]
+        public void Top()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            stack.Push(7);
+            Assert.AreEqual(7, stack.Top());
+        }
+
+        [TestMethod]
+        public void TopException()
+        {
+            Stack<int> stack = new Stack<int>(10);
+            Action act = () => stack.Top();
+            act.Should().Throw<InvalidOperationException>()
+            .WithMessage("Стек пуст. Нет элементов для получения.");
+        }
+
+        [TestMethod]
+        public void PopException()
+        {
+            Stack<int> stack = new Stack<int>(10);
+            Action act = () => stack.Pop();
+            act.Should().Throw<InvalidOperationException>()
+            .WithMessage("Стек пуст. Нет элементов для получения.");
+        }
+
+        [TestMethod]
+        public void PushException()
+        {
+            Stack<string> stack = new Stack<string>(2);
+            Action act = () => {
+                stack.Push("Hello");
+                stack.Push("my");
+                stack.Push("friends");
+            };
+            act.Should().Throw<InvalidOperationException>()
+            .WithMessage("Стек переполнен. Невозможно добавить элемент.");
+        }
+
+        [TestMethod]
+        public void GetEnumeratorFromTopToBottom()
+        {
+            Stack<string> stack = new Stack<string>(3);
+            IEnumerator numerator = stack.GetEnumerator();
+            stack.Push("Hello");
+            stack.Push("Viktoria");
+            stack.Push("Goodbye");
+            numerator.Current.Should().BeNull();
+            numerator.MoveNext();
+            numerator.Current.Should().Be("Goodbye");
+            numerator.MoveNext();
+            numerator.Current.Should().Be("Viktoria");
+            numerator.MoveNext();
+            numerator.Current.Should().Be("Hello");
+        }
+        [TestMethod]
+        public void GetEnumeratorFromEmptyStack()
+        {
+            Stack<string> stack = new Stack<string>(1);
+            IEnumerator numerator = stack.GetEnumerator();
+            numerator.MoveNext().Should().BeFalse();
+            numerator.Current.Should().BeNull();
+        }
+        [TestMethod]
+        public void GetEnumeratorFromFilledStack()
+        {
+            Stack<string> stack = new Stack<string>(1);
+            IEnumerator numerator = stack.GetEnumerator();
+            stack.Push("Hello");
+            numerator.MoveNext();
+            numerator.MoveNext().Should().BeFalse();
+        }
+    }
+}
---
+++ Lections/03_Unit_tests/Library/Stack.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Wintellect.PowerCollections
+{
+    /// <summary>
+    /// Простой пример реализации структуры данных стек нативными средствами языка C#.
+    /// </summary>
+    public class Stack<T> : IEnumerable<T>
+    {
+        private readonly T[] _Array;
+
+        /// <summary>
+        /// Маркер верхнего элемента стека
+        /// </summary>
+        private int Peak { get; set; }
+
+        /// <summary>
+        /// Размер стека
+        /// </summary>
+        public int Capacity { get; }
+
+        /// <summary>
+        /// Количество элементов стека
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return Peak;
+            }
+        }
+
+        /// <summary>
+        /// Конструктор. Создание стек
+        /// </summary>
+        public Stack(int size)
+        {
+            Capacity = size;
+            Peak = 0;
+            _Array = new T[Capacity];
+        }
+
+        /// <summary>
+        /// Проверить заполнен ли стек
+        /// </summary>
+        public bool IsFull()
+        {
+            return Peak == Capacity;
+        }
+
+        /// <summary>
+        /// Проверить пустой ли стек
+        /// </summary>
+        public bool IsEmpty()
+        {
+            return Peak == 0;
+        }
+
+        /// <summary>
+        /// Добавляем элемент в стек
+        /// </summary>
+        public void Push(T item)
+        {
+            if (IsFull())
+                throw new InvalidOperationException("Стек переполнен. Невозможно добавить элемент.");
+
+            _Array[Peak++] = item;
+        }
+
+        /// <summary>
+        /// Считывание из стека верхнего элемента и удаление этого элемента
+        /// </summary>
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Стек пуст. Нет элементов для получения.");
+            }
+            return _Array[--Peak];
+        }
+
+        /// <summary>
+        /// Считывание из стека верхнего элемента
+        /// </summary>
+
+        public T Top()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Стек пуст. Нет элементов для получения.");
+            }
+            return _Array[Peak - 1];
+        }
+
+        /// <summary>
+        /// Считываем стек "от вершины к низу"
+        /// </summary>
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (IsEmpty()) yield break;
+
+            for (int i = Peak; i > 0; i--)
+                yield return _Array[i - 1];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
